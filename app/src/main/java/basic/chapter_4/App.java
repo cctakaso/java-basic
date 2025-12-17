a5b47b05ca2166b458c816bd262f995b9d9d0f8d
takashi ogisawa <t.ogisawa@creeinter.net>
2025-12-17 18:46:43 +0900

    if 文

--- app/src/main/java/basic/chapter_4/App.java
+++ app/src/main/java/basic/chapter_4/App.java
@@ -1,5 +1,6 @@
 package basic.chapter_4;
 
+import java.util.Scanner;
 /**
  * 第4章: 場合分けを考えよう！条件分岐 if文
  *
@@ -11,10 +12,14 @@ package basic.chapter_4;
 public class App {
 
     public static void main(String[] args) {
-        int score = 75;
-        String subject = "Math";
+        // --- キーボードからの入力  ---
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("貴方の数学の点数を入力してください: "); // printはprintlnと違い改行しない
+        int score = scanner.nextInt();
+        scanner.close();
 
-        System.out.println("科目: " + subject + ", 点数: " + score + "点");
+
+        System.out.println("科目: 数学, 点数: " + score + "点");
 
         // --- if-else if-else 文による成績判定 ---
         if (score >= 90) {
@@ -23,20 +28,13 @@ public class App {
             System.out.println("評価: 良");
         } else if (score >= 60) {
             System.out.println("評価: 可");
+        } else if (score >= 50) {
+            System.out.println("評価: 追試");
         } else {
             System.out.println("評価: 不可");
         }
 
         // --- 複雑な条件式 ---
-        // 「数学」で、かつ、点数が60点以上なら「追試なし」
-        if (subject.equals("Math") && score >= 60) {
-            System.out.println("数学は合格です。");
-        }else{
-            System.out.println("数学は不合格です。");
-        }
-        // 三項演算子を使った同じ条件の表現
-        System.out.println(subject.equals("Math") && score >= 60 ? "数学は合格です。" : "数学は不合格です。");
-
         // 点数が100点、または、0点だった場合に特別なメッセージを表示
         if (score == 100 || score == 0) {
             System.out.println("特徴的な点数です！");
@@ -45,5 +43,17 @@ public class App {
         }
         // 三項演算子を使った同じ条件の表現
         System.out.println(score == 100 || score == 0 ? "特徴的な点数です！" : "通常の点数です。");
+
+        // 点数が50点以上60点以下なら「追試」
+        if (50 <= score && score < 60) {
+            System.out.println("数学は追試です。");
+        }else if (score < 50) {
+            System.out.println("数学は不合格です。");
+        }else{
+            System.out.println("数学は合格です。");
+        }
+        // 三項演算子を使った同じ条件の表現
+        System.out.println(50 <= score && score < 60 ? "数学は追試です。" : score < 50 ? "数学は不合格です。": "数学は合格です。");
+
     }
 }
